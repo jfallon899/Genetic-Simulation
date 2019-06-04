1ed830b3ff78b4e7dc205b9de916082eee0c5893
John Fallon <jfallon899@gmail.com>
2019-06-04 11:33:23 -0400

    learning to use github

---
+++ hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 11:06:20 2019
+
+@author: jfal8
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
